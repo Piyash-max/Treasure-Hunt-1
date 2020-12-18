46cd1b11f05fa89ffcdc3f77c6955c9e359f2618
Piyash-max <71182127+Piyash-max@users.noreply.github.com>
2020-12-18 21:04:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var gameSate="Welcome";
 var welcome,welbg,gameover,gobg;
 var  ground,ground1,ground2,ground3,ground4;
 var skeppy,skeppy1,skeppy2,skeppy3,skeppy4,skeppy5;
@@ -7,12 +8,12 @@ var boss,boss1,boss2,boss3,boss4;
 var fireball,fireballimg;
 var button,buttonimg;
 function preload(){
- welbg=loadImage("images/WelcomeImage.jpg");
+ welbg=loadAnimation("images/WelcomeImage.jpg");
  gobg=loadImage("images/GameOver.png");
- ground1=loadImage("images/Background1.jpg");
- ground2=loadImage("images/Background2.jpg");
- ground3=loadImage("images/Background3.jpg");
- ground4=loadImage("images/Background4.jpg");
+ ground1=loadAnimation("images/Background1.jpg");
+ ground2=loadAnimation("images/Background2.jpg");
+ ground3=loadAnimation("images/Background3.jpg");
+ ground4=loadAnimation("images/Background4.jpg");
  skeppy1=loadImage("images/Player1.png");
  skeppy2=loadImage("images/Player2.png");
  skeppy3=loadImage("images/Player3.png");
@@ -34,22 +35,40 @@ function preload(){
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  welcome=createSprite(800,370,windowWidth,windowHeight);
   ground=createSprite(800,370,windowWidth,windowHeight);
-  ground.addImage(welbg);
+  ground.scale=3.5;
   button=createSprite(800,600);
   button.addImage(buttonimg)
   button.scale=0.5;
- 
+  skeppy=createSprite(50,height-10,50,50);
 }
 
 function draw() {
   background(255,255,255);  
-  if (mouseIsPressed) {
-    ground.changeImage(ground);
-  //  lol=createSprite(800,370,windowWidth,windowHeight);
-  //  lol.addImage(ground1);
-  //  lol.scale=4;
+  if(gameSate==="Welcome"){
+    welcome.addAnimation("Welcome",welbg);
+    if(mousePressedOver(button)){
+      ground.addAnimation("grnd",ground1);
+      skeppy.addImage(skeppy1);
+      welcome.visible=false;
+      button.visible=false;
+      gameSate="Play";
+   }
   } 
+  
+  else if(gameSate==="Play"){
+    ground.velocityX=-4;
+    if(ground.x < 700){
+      ground.x=ground.width/2;
+      skeppy.addImage(skeppy1);
+      ground.addAnimation("grnd",ground1);
+
+  }
+ 
+  
+  
   drawSprites();
  
+}
 }
