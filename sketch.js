eeea77f8731a69833b8e7f47c6d41b6f6f68280d
Piyash-max <71182127+Piyash-max@users.noreply.github.com>
2020-12-27 18:23:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var gameSate="Welcome";
+var gameSate=0;
 var welcome,welbg,gameover,gobg;
 var  ground,ground1,ground2,ground3,ground4;
 var skeppy,skeppy1,skeppy2,skeppy3,skeppy4,skeppy5;
@@ -7,6 +7,8 @@ var chest,chest1,chest2;
 var boss,boss1,boss2,boss3,boss4;
 var fireball,fireballimg;
 var button,buttonimg;
+var swordimg,sword;
+var EnemyGroup;
 function preload(){
  welbg=loadAnimation("images/WelcomeImage.jpg");
  gobg=loadImage("images/GameOver.png");
@@ -32,13 +34,20 @@ function preload(){
  chest2=loadImage("images/Chest2.png");
  fireballimg=loadImage("images/FireBall.png");
  buttonimg=loadImage("images/start.png");
+ swordimg=loadImage("images/PlayerItem.png");
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  EnemyGroup=createGroup();
   welcome=createSprite(800,370,windowWidth,windowHeight);
   welcome.addAnimation("welcome",welbg);
+  welcome.scale=1.2;
   welcome.visible=false;
   welcome.addAnimation("grnd",ground1);
+  skeppy=createSprite(200,height-170,50,50);
+  skeppy.addImage("player1",skeppy4);
+  skeppy.scale=0.75; 
+  skeppy.visible=false;
   button=createSprite(800,600);
   button.addImage(buttonimg)
   button.scale=0.5;
@@ -48,20 +57,17 @@ function setup() {
 
 function draw() {
   background(ground1);  
-  if(gameSate==="WELCOME"){
+  if(gameSate===0){
     welcome.visible=true;
     button.visible=true;
   if(mousePressedOver(button)){
-    gameSate="PLAY";
+    gameSate=1;
   }
 }
- if(gameSate==="PLAY"){
+ if(gameSate===1){
   welcome.visible=false;
   button.visible=false;
-  skeppy=createSprite(200,height-170,50,50);
-  skeppy.addImage("player1",skeppy4);
-  skeppy.scale=0.75; 
-
+  skeppy.visible=true;
   
   if(keyDown(UP_ARROW)){
     skeppy.y=skeppy.y-10;
@@ -69,6 +75,31 @@ function draw() {
   if(keyDown(DOWN_ARROW)){
     skeppy.y=skeppy.y+10;   
   }
+  if(keyDown("SPACE")){
+    var temp_sword=createSword();
+    temp_sword.addImage(swordimg);
+    temp_sword.y=skeppy.y;
+  }
+  Enemy();
 }
   drawSprites();
+}
+
+function createSword(){
+ sword=createSprite(50,50,5,10)
+ sword.velocityX=6;
+ sword.scale=0.3;
+ return sword;
+}
+function Enemy(){
+  if (World.frameCount%10 ===0){
+    var enemy = createSprite(windowWidth,windowHeight/2,20,20);
+    enemy.addImage(enemy1);
+    enemy.y=Math.round(random(windowWidth,500));
+    enemy.scale=0.5;
+    enemy.velocityX=-9;
+    enemy.lifetime=300;
+    
+    EnemyGroup.add(enemy);
+}
 }
